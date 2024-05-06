2e9149eb927222608fa57198364356f9f7eac9ae
willfjunior <wfrutuosojunior@gmail.com>
2024-05-06 07:11:27 -0300

    ajuste lank context

--- Infra.Data/LankContext.cs
+++ Infra.Data/LankContext.cs
@@ -5,6 +5,11 @@ namespace Infra.Data
     public  class LankContext
     {
         private readonly IConfiguration _configuration;
+        public string Server { get; set; }
+        public string Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
 
         public LankContext(IConfiguration configuration)
         {
@@ -13,7 +18,13 @@ namespace Infra.Data
 
         public string GetConnectionString()
         {
-            return _configuration.GetConnectionString("Default");
+            Server = _configuration.GetSection("EnvDB:Server").Value;
+            Port = _configuration.GetSection("EnvDB:Port").Value;
+            Username = _configuration.GetSection("EnvDB:UserName").Value;
+            Password = _configuration.GetSection("EnvDB:Password").Value;
+            Database = _configuration.GetSection("EnvDB:DataBase").Value;
+            
+            return $"Server={Server};Port={Port};Uid={Username};Pwd={Password};Database={Database}";
         }
     }
 }
